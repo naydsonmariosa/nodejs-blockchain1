f425c95d4738b02ee9b452ede62a5af8cf006a71
Naydson <naydsonmariosa@hotmail.com>
2022-04-19 19:14:59 -0300

    Code review , replacing filter by find method as suggested by the tutor. Resulted as a more clean code

--- src/blockchain.js
+++ src/blockchain.js
@@ -86,7 +86,7 @@ class Blockchain {
                     return;
                 }
                 else {
-                    reject(`Error on add block. Block's data corruption.`);
+                    reject(`Error on add block. Block's data .`);
                 }
            }
 
@@ -181,7 +181,7 @@ class Blockchain {
     getBlockByHeight(height) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.height === height)[0];
+            let block = self.chain.find(p => p.height === height);
             if(block){
                 resolve(block);
             } else {
